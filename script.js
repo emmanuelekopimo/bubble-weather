2996f6ca09d80d5ac951c07f4f092f66a49a2905
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2023-04-11 23:03:26 -0700

    Code update, Folder restructuring

--- script.js
+++ script.js
@@ -7,8 +7,74 @@ const scrollArea = document.querySelector('.scroll-area')
 
 // Weather elements
 const currentWeatherIcon = document.querySelector('.current-weather-icon')
+const currentTemperature = document.querySelector('.temperature.current')
+const detailWind = document.querySelector('.detail.wind')
+const detailHumidity = document.querySelector('.detail.humidity')
+const detailRain = document.querySelector('.detail.rain')
 
 var lastCity = ''
+const gps = {
+    latitude: 0,
+    longitude: 0
+}
+
+// Periodic Updating of weather
+const updateWeather = () => {
+    if (lastCity.length > 0){
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${gps.latitude}&longitude=${gps.longitude}&hourly=temperature_2m,weathercode&current_weather=true`)
+            .then(res => {
+                return res.json()
+            })
+            .then(data =>{
+                currentTemperature.innerText = parseFloat(data.current_weather.temperature)
+                    .toFixed(0)
+                    .toString()
+                detailWind.innerText = `${parseFloat(data.current_weather.windspeed)
+                    .toFixed(0)
+                    .toString()} Km/h `
+                console.log(data.current_weather.weathercode)
+                switch(data.current_weather.weathercode){
+                    case 0:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/sun.png')
+                        break
+                    case 1:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/sun.png')
+                        break
+                    case 2: 
+                        currentWeatherIcon.setAttribute('src', './icons/weather/sun-cloud.png.png')
+                        break
+                    case 3:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/cloud.png')
+                        break
+                    case 45: case 48:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/cloud.png')
+                        break
+                    case 51: case 53: case 55: case 56: case 57: case 61: 
+                    case 63: case 65: case 66: case 67:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/rain.png')
+                        break
+                    case 71: case 73: case 75: case	77:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/snow.png')
+                        break
+                    case 80: case 81: case 82:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/rain.png')
+                        break
+                    case 85: case 86:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/snow.png')
+                        break
+                    case 95: case 96: case 99:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/storm.png')
+                        break
+                    default:
+                        currentWeatherIcon.setAttribute('src', './icons/weather/sun.png')
+
+
+                }
+            })
+    }
+}
+setInterval (updateWeather, 10000) // Every 10 Seconds
+
 searchBar.spellCheck = false
 searchBar.onfocus = () => {
     currentAction.setAttribute('src','./icons/pencil.png')
@@ -41,34 +107,49 @@ searchBar.onblur = () => {
 
 searchBar.oninput = () => {
     // Where search comes in
-    let data = {"results":[{"id":220428,"name":"Akwa","latitude":3.44646,"longitude":29.42378,"elevation":800.0,"feature_code":"PPL","country_code":"CD","admin1_id":216140,"admin2_id":12278089,"timezone":"Africa/Lubumbashi","country_id":203312,"country":"DR Congo","admin1":"Haut-Uele","admin2":"Faradje"},{"id":2235749,"name":"Akwa","latitude":5.3519,"longitude":8.9356,"elevation":286.0,"feature_code":"PPL","country_code":"CM","admin1_id":2221788,"timezone":"Africa/Douala","country_id":2233387,"country":"Cameroon","admin1":"South-West"},{"id":2350821,"name":"Akwa","latitude":5.91172,"longitude":7.01132,"elevation":60.0,"feature_code":"PPL","country_code":"NG","admin1_id":2349961,"admin2_id":7701534,"timezone":"Africa/Lagos","country_id":2328926,"country":"Nigeria","admin1":"Anambra","admin2":"Nnewi South"},{"id":2350822,"name":"Akwa","latitude":5.0292,"longitude":7.15748,"elevation":36.0,"feature_code":"PPL","country_code":"NG","admin1_id":2324433,"admin2_id":7701519,"timezone":"Africa/Lagos","country_id":2328926,"country":"Nigeria","admin1":"Rivers State","admin2":"Etche"},{"id":6855199,"name":"Akwa","latitude":6.05348,"longitude":9.47493,"elevation":133.0,"feature_code":"PPL","country_code":"CM","admin1_id":2221788,"timezone":"Africa/Douala","country_id":2233387,"country":"Cameroon","admin1":"South-West"},{"id":10814485,"name":"Akwa","latitude":29.83626,"longitude":79.65391,"elevation":2097.0,"feature_code":"PPL","country_code":"IN","admin1_id":1444366,"admin2_id":7701513,"timezone":"Asia/Kolkata","country_id":1269750,"country":"India","admin1":"Uttarakhand","admin2":"Bageshwar"},{"id":2235748,"name":"Akwa","latitude":4.05,"longitude":9.7,"elevation":18.0,"feature_code":"PPLX","country_code":"CM","admin1_id":2229336,"timezone":"Africa/Douala","country_id":2233387,"country":"Cameroon","admin1":"Littoral"},{"id":379761,"name":"Akwat","latitude":9.051,"longitude":32.41661,"elevation":400.0,"feature_code":"PPL","country_code":"SS","admin1_id":381229,"timezone":"Africa/Juba","country_id":7909807,"country":"South Sudan","admin1":"Upper Nile State"},{"id":379774,"name":"Akwar","latitude":9.1006,"longitude":26.72356,"elevation":456.0,"feature_code":"PPL","country_code":"SS","admin1_id":408665,"timezone":"Africa/Juba","country_id":7909807,"country":"South Sudan","admin1":"Northern Bahr al Ghazal"},{"id":379776,"name":"Akwaj","latitude":10.3868,"longitude":32.61437,"elevation":390.0,"feature_code":"PPL","country_code":"SS","admin1_id":381229,"timezone":"Africa/Juba","country_id":7909807,"country":"South Sudan","admin1":"Upper Nile State"}],"generationtime_ms":0.3979206}
-    let resultStack =''
-    let resultsList = data.results.slice(0,5)
-    for (let searchResult of resultsList){
-        admin2 = searchResult.admin2===undefined?'':searchResult.admin2+','
-        resultStack += `<div class="drop-down-item">
-        ${searchResult.name}, ${admin2} ${searchResult.admin1}, ${searchResult.country}
-        </div>`
-    }
-    
-
-    dropDown.innerHTML = resultStack
-    let totalHeight = 0
-    
-    document.querySelectorAll('.drop-down-item').forEach((element)=>{
-            totalHeight += element.offsetHeight
-            element.onclick = () => {
-                lastCity = element.innerText
-                searchBar.value = lastCity
-                // Latency effect to show selected option
-                setTimeout(() => {
-                    dropDown.style.visibility = 'hidden'
-                }, 100)
-            }
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchBar.value}`)
+        .then(res => {
+            return res.json();
         })
-    dropDown.style.height = `${totalHeight}px`
+        .then(data => {
+            let resultStack =''
+            let resultsList = data.results.slice(0,5)
+            for (let searchResult of resultsList){
+                admin2 = searchResult.admin2===undefined?'':searchResult.admin2+','
+                resultStack += `<div class="drop-down-item" lat="${searchResult.latitude}" long="${searchResult.longitude}">
+                ${searchResult.name}, ${admin2} ${searchResult.admin1}, ${searchResult.country}
+                </div>`
+            }
+            dropDown.innerHTML = resultStack
+                    // Set new height 
+            let totalHeight = 0
+            
+            document.querySelectorAll('.drop-down-item').forEach((element)=>{
+                    totalHeight += element.offsetHeight
+                    element.onclick = () => {
+                        lastCity = element.innerText
+                        searchBar.value = lastCity
+                        gps.latitude = element.getAttribute('lat')
+                        gps.longitude = element.getAttribute('long')
+                        updateWeather()
+                        // Latency effect to show selected option
+                        setTimeout(() => {
+                            dropDown.style.visibility = 'hidden'
+                        }, 100)
+                    }
+                })
+            dropDown.style.height = `${totalHeight}px`
+        })
+        .catch(
+            err => {
+                console.log(err)
+            }
+        )
+    
 
 }
+
+
 
 var scrollingRight = false
 var scrollingLeft = false
