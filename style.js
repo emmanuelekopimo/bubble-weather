681ccfab808e9bd15c01575fcbbc5c7cb704d3e8
Emmanuel Ekopimo <emmanuelekopimo@gmail.com>
2023-04-14 11:48:35 -0700

    Fixed mobile layout

--- style.js
+++ style.js
@@ -1,21 +1,20 @@
 // Elements
-const main = document.querySelector('.main')
-const locationBar = document.querySelector('.location-bar')
-const temperature = document.querySelector('.temperature')
-const temperatureContainer = document.querySelector('.temperature-container')
+const main = document.querySelector(".main");
+const locationBar = document.querySelector(".location-bar");
+const temperature = document.querySelector(".temperature");
+const temperatureContainer = document.querySelector(".temperature-container");
 
 const resize = () => {
-    main.style.width = `${innerWidth-50}px`
-    main.style.height = `${innerHeight-130}px`
+  main.style.width = `${innerWidth - 50}px`;
+  main.style.height = `${innerHeight - 130}px`;
 
-    if (innerWidth <= 420){
-        main.style.width = `${innerWidth-10}px`
-        main.style.height = `${innerHeight-180}px`
-        locationBar.style.width = `${innerWidth-34}px`
-    } 
-
-}
+  if (innerWidth <= 420) {
+    main.style.width = `${innerWidth - 36}px`;
+    main.style.height = `${innerHeight - 180}px`;
+    locationBar.style.width = `${innerWidth - 34}px`;
+  }
+};
 
 // All things that happen on resize
-window.addEventListener('resize', resize)
-resize()
+window.addEventListener("resize", resize);
+resize();
